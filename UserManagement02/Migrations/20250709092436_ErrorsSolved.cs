9b7b80d3bd701cdf1b13ff770ece5c9b17b5acb0
AlomaryAli <AlomaryAli29@gmail.com>
2025-07-09 13:57:42 +0300

    Supervisor files were added

---
+++ UserManagement02/Migrations/20250709092436_ErrorsSolved.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement02.Migrations
+{
+    /// <inheritdoc />
+    public partial class ErrorsSolved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Supervisors",
+                table: "Supervisors");
+
+            migrationBuilder.DropColumn(
+                name: "SupervisorId",
+                table: "Supervisors");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Supervisors",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Supervisors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Supervisors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Supervisors",
+                table: "Supervisors",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Supervisors",
+                table: "Supervisors");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Supervisors",
+                newName: "DepartmentId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Supervisors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Supervisors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupervisorId",
+                table: "Supervisors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Supervisors",
+                table: "Supervisors",
+                column: "SupervisorId");
+        }
+    }
+}
---
+++ UserManagement02/Migrations/20250709093318_ErrorsSolved01.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement02.Migrations
+{
+    /// <inheritdoc />
+    public partial class ErrorsSolved01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AppUserId",
+                table: "AppUsers",
+                newName: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserID",
+                table: "AppUsers",
+                newName: "AppUserId");
+        }
+    }
+}
